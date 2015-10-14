6d5f32d16b15786966edbf979eaf1cd13f3e4cb7
Gamebreack <victorlescan@gmail.com>
2015-10-13 21:38:05 -0300

    Create example_half_step.ino

---
+++ example_half_step/example_half_step.ino
@@ -0,0 +1,25 @@
+#include <motorGB.h>
+motorGB mt1(9,12,10,11);// pinos do motor 1, 2, 3 e 4, 
+
+void setup()
+{
+  mt1.setspeed(10);// velocidade(por enquanto não é em RPM)
+  mt1.setRevolution(50);// número de passos por revolução
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop()
+{
+  ////////////////
+  mt1.run_half(5,0);// número de voltas, direção
+  delay(1000);
+
+  mt1.run_half(5,1);
+  delay(1000);
+
+  mt1.doRevolution_half(0);//direção
+  delay(1000);
+
+  mt1.doRevolution_half(1);  
+  delay(1000);
+  ////////////////  
+}
